debug softmax applying on wrong axis

chemvae_train/models.py
@@ -305,7 +305,6 @@ class SamplingLayer(Layer):
 # The following is the translated AE in pytorch:
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from chemvae_train.load_params import ChemVAETrainingParams
 from chemvae_train.models_utils import add_activation
 
@@ -478,6 +477,7 @@ class Decoder(nn.Module):
             x = middle(x)
         x = self.z_repeats(x)
 
+        # from this point onwards, the shape of x is [batch_size, sequence_length (data_width), in_channel (data_width)]
         for gru, acti in zip(self.gru_layers, self.gru_activation_layers):
             x, _ = gru(x)
             x = acti(x)
@@ -504,7 +504,7 @@ class Decoder(nn.Module):
 
 
 def variational_layers(z_mean, z_log_var):
-    epsilon = torch.randn(z_mean.size(), device=z_mean.device) 
+    epsilon = torch.randn(z_mean.size(), device=z_mean.device)
     z_rand = z_mean + torch.exp(z_log_var / 2) * epsilon
     return z_rand
 

chemvae_train/models_utils.py
@@ -14,7 +14,7 @@ def add_activation(activation_param):
     elif activation_param == 'relu':
         return nn.ReLU()
     elif activation_param == 'softmax':
-        return nn.Softmax(dim=1)
+        return nn.Softmax(dim=2)
     else:
         raise ValueError('Invalid activation function specified in the params file.')
 
@@ -106,9 +106,9 @@ def categorical_crossentropy_onehot(pred, target):
     Returns:
         Tensor: Mean categorical cross-entropy loss.
     """
-    # Apply log to predictions
-    log_probs = torch.log(pred + 1e-8)  # Avoid log(0)
 
+    pred = torch.nn.functional.softmax(pred, dim=1)  # Apply softmax along the class dimension
+    log_probs = torch.log(pred + 1e-8)  # Avoid log(0)
     # Multiply log_probs with one-hot targets and sum over the class axis
     loss = -torch.sum(target * log_probs, dim=1)  # Sum over the class dimension
 