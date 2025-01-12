90ff28c42967cb5d4d3e81e064eb1c1b4a1369a1
iris-ywang <iris_ywang@outlook.com>
2025-01-12 10:54:20 +0000

    option to save logging file or not

--- utils/utils.py
+++ utils/utils.py
@@ -2,16 +2,22 @@ import logging
 from datetime import datetime
 
 
-def logging_set_up(logging_filename_prefix: str = None):
+def logging_set_up(save_log_file: bool = True, logging_filename_prefix: str = None):
     # config logging to be compatible with the pytorch
     if (logging_filename_prefix is not None) & (not logging_filename_prefix.endswith("_")):
         logging_filename_prefix = f"{logging_filename_prefix}_"
     else:
         logging_filename_prefix = ""
 
+    if save_log_file:
+        filename = f"{logging_filename_prefix}loggings_on_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt"
+    else:
+        filename = None
     logging.basicConfig(
-        level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
-        filename=f"{logging_filename_prefix}loggings_on_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt",
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filename=filename,
     )
     # set up logging to console
     console = logging.StreamHandler()
