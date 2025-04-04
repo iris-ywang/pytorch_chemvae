(VOID ALL THE RESULT BEFORE THIS COMMIT) 1. fix VAE weighting via epoch issue 2. added eval with extrapolation metrics 3. enabled saving optimiser

model_evaluations/pa_eval_utils.py
@@ -32,14 +32,13 @@ def run(
                 target_value_col_name=target_value_col_name,
             )
             metrics_per_fold["fold_id"] = fold_id
-            logging.info(f"Metrics for fold {fold_id}: {metrics_per_fold}")
+            logging.info(f"Metrics for fold {fold_id}: \n {metrics_per_fold.to_string()}")
             metrics_per_dataset.append(metrics_per_fold)
 
         all_metrics = pd.concat(metrics_per_dataset)
         mean_metrics = all_metrics.drop(columns=["fold_id"]).groupby(all_metrics.index).mean().head()
-        print(all_metrics)
         print()
-        print(mean_metrics)
+        print(mean_metrics.to_string())
 
         return mean_metrics, all_metrics
 
@@ -109,7 +108,7 @@ def results_of_pairwise_combinations(
         rank_method=rating_trueskill,
         percentage_of_top_samples=0.1,
 ):
-    results_dict = {}
+    results_df = pd.DataFrame()
     if pairwise_model.ML_cls is not None:
         logging.info("Extrapolation performance evaluation...")
         y_ranking_c2 = pairwise_model.predict_rank(
@@ -123,7 +122,7 @@ def results_of_pairwise_combinations(
             y_train_with_predicted_test=y_ranking_c2,
             pairwise_data_info=pairwise_model.pairwise_data_info,
         ).run_extrapolation_evaluation()
-        results_dict["rank_metrics_c2"] = metrics_c2
+        results_df["rank_metrics_c2"] = metrics_c2
 
         y_ranking_c2_c3 = pairwise_model.predict_rank(
             ranking_method=rank_method,
@@ -136,7 +135,7 @@ def results_of_pairwise_combinations(
             y_train_with_predicted_test=y_ranking_c2_c3,
             pairwise_data_info=pairwise_model.pairwise_data_info,
         ).run_extrapolation_evaluation()
-        results_dict["rank_metrics_c2_c3"] = metrics_c2_c3
+        results_df["rank_metrics_c2_c3"] = metrics_c2_c3
 
         y_ranking_c1_c2_c3 = pairwise_model.predict_rank(
             ranking_method=rank_method,
@@ -149,13 +148,13 @@ def results_of_pairwise_combinations(
             y_train_with_predicted_test=y_ranking_c1_c2_c3,
             pairwise_data_info=pairwise_model.pairwise_data_info,
         ).run_extrapolation_evaluation()
-        results_dict["rank_metrics_c1_c2_c3"] = metrics_c1_c2_c3
+        results_df["rank_metrics_c1_c2_c3"] = metrics_c1_c2_c3
 
         # Regressive prediction performance evaluation:
-        if not if_rank_with_dist:
-            metrics_est = [np.nan for _ in range(6)]
-
-        results_dict["reg_metrics_from_sbbr"] = metrics_est
+        # if not if_rank_with_dist:
+        #     metrics_est = [np.nan for _ in range(6)]
+        #
+        # results_dict["reg_metrics_from_sbbr"] = metrics_est
 
     if pairwise_model.ML_reg is not None:
         logging.info("Pairwise regressive performance evaluation...")
@@ -172,8 +171,10 @@ def results_of_pairwise_combinations(
             pairwise_model.pairwise_data_info.test_ary[:, 0],
             y_est
         )
-        results_dict["reg_metrics_c2"] = metrics_est
-    return results_dict
+        metrics_est.name = "reg_metrics_c2"
+        results_df = pd.concat([results_df, metrics_est], axis=1)
+
+    return results_df
 
 
 def metrics_evaluation(y_true, y_predict):
@@ -182,7 +183,7 @@ def metrics_evaluation(y_true, y_predict):
     mae = mean_absolute_error(y_true, y_predict)
     r2 = r2_score(y_true, y_predict)
     # return [rho, mse, mae, r2, np.nan, np.nan]
-    return {"rho": rho, "mse": mse, "mae": mae, "r2": r2}
+    return pd.Series({"rho": rho, "mse": mse, "mae": mae, "r2": r2})
 
 
 def estimate_y_from_averaging(Y_pa_c2, c2_test_pair_ids, test_ids, y_true, Y_weighted=None):

submodules/pairwise_formulation/evaluations/extrapolation_evaluation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.metrics import precision_score, recall_score, f1_score
 from submodules.pairwise_formulation.pairwise_data import PairwiseDataInfo
 
@@ -76,7 +77,20 @@ class ExtrapolationEvaluation:
                 tests_better_than_top_train_true, tests_better_than_top_train
             )
 
-            return [precision_top, recall_top, f1_top,
-                    precision_better, recall_better, f1_better]
+            return pd.Series({
+                "precision_top": precision_top,
+                "recall_top": recall_top,
+                "f1_top": f1_top,
+                "precision_better": precision_better,
+                "recall_better": recall_better,
+                "f1_better": f1_better
+            })
         else:
-            return [np.nan for _ in range(6)]
+            return pd.Series({
+                "precision_top": np.nan,
+                "recall_top": np.nan,
+                "f1_top": np.nan,
+                "precision_better": np.nan,
+                "recall_better": np.nan,
+                "f1_better": np.nan
+            })

train_vae.py
@@ -7,7 +7,6 @@ import numpy as np
 import torch
 import logging
 
-import torch.multiprocessing as mp
 from torch import nn
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
@@ -86,7 +85,7 @@ def load_model(params: ChemVAETrainingParams, evaluating=False):
 
     if params.reload_model or evaluating:
 
-        if params.pre_trained_weights_file is not None and params.loop_over_fit_batch_id == 0:
+        if params.pre_trained_weights_file is not None and params.loop_over_fit_batch_id == 0 and not evaluating:
             weights_path = params.pre_trained_weights_file
         else:
             weights_path = params.vae_weights_file
@@ -108,7 +107,7 @@ def save_model(params, vae_model, batch_id, batch_size_per_loop, gpu_id=None):
         filename = params.vae_weights_file
         chunk_batch_filename = params.vae_weights_file[:-4] + f"_{(batch_id + 1) * batch_size_per_loop}.pth"
         torch.save(vae_model.state_dict(), filename)
-        if params.save_model_per_chunk:
+        if params.save_model_per_epoch:
             torch.save(vae_model.state_dict(), chunk_batch_filename)
         else:
             chunk_batch_filename = ""
@@ -120,6 +119,17 @@ def save_model(params, vae_model, batch_id, batch_size_per_loop, gpu_id=None):
         logging.info(f"Model weights saved to {filename}. \n")
 
 
+def save_optimiser(params, optimizer, gpu_id=None):
+    """Save the optimizer state for the training process. If gpu_id is not None, save only for gpu_id 0."""
+    if torch.cuda.is_available():
+        if gpu_id != 0:
+            return
+    if params.optimiser_file:
+        filename = params.optimiser_file
+        torch.save(optimizer.state_dict(), filename)
+        logging.info(f"Optimizer state saved to {filename}. \n")
+
+
 def train(params: ChemVAETrainingParams):
     """Train the ChemVAE model, the full workflow."""
     # set device to cuda of id = gpu_id if available, else to cpu
@@ -127,8 +137,9 @@ def train(params: ChemVAETrainingParams):
         local_rank = int(os.environ["LOCAL_RANK"])
         global_rank = int(os.environ["RANK"])
     else:
+        local_rank = None
         global_rank = "None"
-    device = torch.device(f"cuda:{local_rank}" if torch.cuda.is_available() else "cpu")
+    device = torch.device(f"cuda:{local_rank} out of {global_rank}" if torch.cuda.is_available() else "cpu")
     logging.info(f"Device: {device}")
 
     # Load data
@@ -139,6 +150,11 @@ def train(params: ChemVAETrainingParams):
         data_preprocessor.get_model_fit_chunk_size_and_starting_chunk_id(params)
 
     data_preprocessor.generate_training_chunks(params, n_chunks)
+
+    epochs_per_chunk = params.epochs
+    total_global_epochs = epochs_per_chunk * n_chunks
+    logging.info(f"Total number of epochs: {total_global_epochs}.")
+    epoch_start_id = chunk_start_id * epochs_per_chunk
 
     if params.paired_output:
         data_preprocessor.generate_fixed_test_pairs(chunk_size_per_loop, random_state=params.RAND_SEED)
@@ -162,7 +178,9 @@ def train(params: ChemVAETrainingParams):
     kl_weight = params.kl_loss_weight  # Initial weight for KL loss
     weight_annealer = WeightAnnealer(
         schedule=lambda epoch: sigmoid_schedule(
-            epoch, slope=params.anneal_sigmod_slope, start=params.vae_annealer_start
+            epoch,
+            slope=30 / total_global_epochs,
+            start=total_global_epochs / 2.5,
         ),
         weight_var=kl_weight,
         weight_orig=kl_weight
@@ -174,27 +192,28 @@ def train(params: ChemVAETrainingParams):
 
     # ##
     # Training loop - chunk by chunk
-    for chunk_id in range(chunk_start_id, n_chunks):
-        print(f"Training batch id over model fit func: {chunk_id} out of {n_chunks}")
+    for epoch in range(epoch_start_id, total_global_epochs):
+        print(f"Training epoch {epoch} out of {total_global_epochs}.")
+        weight_annealer.on_epoch_begin(epoch)
 
-        # load chunk size data
-        data_preprocessor.X_all = None  # clear memory
-        X_train_chunk = data_preprocessor.generate_loop_chunk_data_for_model_fit(
-            if_paired=params.paired_output,
-            current_chunk_id=chunk_id,
-        )
-        batch_size = params.model_fit_batch_size
-        train_loader = load_data(
-            model_fit_batch_size=batch_size,
-            X=X_train_chunk,
-        )
+        for chunk_id in range(chunk_start_id, n_chunks):
+            print(f"Training batch id over model fit func: {chunk_id} out of {n_chunks}")
 
-        num_train_samples = len(train_loader.dataset)
+            # load chunk size data
+            data_preprocessor.X_all = None  # clear memory
+            X_train_chunk = data_preprocessor.generate_loop_chunk_data_for_model_fit(
+                if_paired=params.paired_output,
+                current_chunk_id=chunk_id,
+            )
+            batch_size = params.model_fit_batch_size
+            train_loader = load_data(
+                model_fit_batch_size=batch_size,
+                X=X_train_chunk,
+            )
 
-        for epoch in range(params.epochs):
+            num_train_samples = len(train_loader.dataset)
+
             train_results = {"loss": [], "x_pred_loss": [], "kl_loss": [], "similarity_loss": [], "categorical_accuracy": []}
-            weight_annealer.on_epoch_begin(epoch)
-
             # for loop over train_loader with both ith batch_idx and ith X data
             for batch_idx, X in enumerate(train_loader):
                 autoencoder_model.train()
@@ -235,7 +254,7 @@ def train(params: ChemVAETrainingParams):
             train_accuracy = sum(train_results["categorical_accuracy"]) / len(train_results["categorical_accuracy"])
 
             print(
-                f"Current chunk: {chunk_id}, epoch: {epoch}, gpu: {global_rank}\n; "
+                f"Current chunk: {chunk_id}, epoch: {epoch}, gpu: {global_rank}: \n "
                 f"Average Train loss: {train_loss}, x_pred_loss: {train_x_pred_loss}, "
                 f"kl_loss: {train_kl_loss}, similarity_loss: {train_similarity_loss}, "
                 f"accuracy: {train_accuracy}.")
@@ -282,6 +301,7 @@ def train(params: ChemVAETrainingParams):
                     epoch_results = {
                         "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                         "epoch": epoch,
+                        "chunk": chunk_id,
                         "loss": train_loss,
                         "val_loss": val_loss,
                         "val_x_pred_loss": val_x_pred_loss,
@@ -302,10 +322,11 @@ def train(params: ChemVAETrainingParams):
                     writer.writerow(epoch_results)
                 print(f"Epoch-level evaluation results on test data type {key}: ", epoch_results)
             print("Evaluation end time: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-            if epoch == 0: log_gpu_stats()
+            if chunk_start_id == chunk_id: log_gpu_stats()
 
         logging.info(f"Training batch id {chunk_id} completed. Saving model weights.")
-        save_model(params, autoencoder_model, chunk_id, chunk_size_per_loop, local_rank)
+        save_model(params, autoencoder_model, optimizer, chunk_id, chunk_size_per_loop, global_rank)
+        save_optimiser(params, optimizer, global_rank)
 
         # clear memory
         del train_loader