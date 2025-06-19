4ac502f63af75f1c6211c6412acd9c1587a5bf93
iris-ywang <iris_ywang@outlook.com>
2025-06-19 12:11:05 +0100

    add batch run for NN model + data

--- eval_pa_chembl.py
+++ eval_pa_chembl.py
@@ -2,6 +2,7 @@ import logging
 import os
 import warnings
 
+import pandas as pd
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.linear_model import LinearRegression
 
@@ -26,10 +27,10 @@ def main(params: ChemVAETrainingParams, n_qsar_test_size=None):
     # pairing_method = LatentRepViaFPVAE(fp_autoencoder, params.hidden_dim).get_latent_rep
 
     # Prepare pairs
-    train_test_splits_dict = kfold_splits(train_test=train_test, fold=10)
+    train_test_splits_dict = kfold_splits(train_test=train_test, fold=3)
     pairing_method = ChEMBLToDeltaYNN.make_pairs_from_all_data_and_pair_ids
 
-    metrics_per_dataset = run(
+    metrics_per_dataset, all_metrics = run(
         train_test_splits_dict=train_test_splits_dict,
         SA_ML_reg=RandomForestRegressor(random_state=1, n_jobs=-1),  # for predecessor comparison
         # ML_reg=LinearRegression(),  # for debugging purpose only
@@ -38,6 +39,7 @@ def main(params: ChemVAETrainingParams, n_qsar_test_size=None):
         pairing_method=pairing_method,
         percentage_of_top_samples=0.1,  # top-performing as in top 10%
     )
+    return metrics_per_dataset
 
 
 def run_eval(
@@ -70,6 +72,81 @@ def run_eval(
     main(training_params, n_test_size)
 
 
+def run_eval_in_batch(list_of_chembl_file_path: list, eval_size: int = 100):
+
+    #################### CHECK
+    fp_siamese_depth = 2
+    fp_concat_depth = 3
+    fp_n_activity_layers = 3
+    fp_activity_layer_size_scalar = 1.0
+    fp_hidden_dim_reduction_rate = 0.5
+    fp_loss_weight = 10.0
+    epochs = 35
+
+    hidden_dim = 392
+    save_model_per_epoch = False
+    ###################
+
+    metrics_all = pd.DataFrame()
+    for data_file_path in list_of_chembl_file_path:
+        df = pd.read_csv(data_file_path, index_col="molecule_id")
+        chembl_dataset_id = data_file_path.split("CHEMBL")[-1].split(".")[0]
+        chembl_name = f"chembl{chembl_dataset_id}"
+
+        ### Get the whole training set from the entire dataset.
+        data_width = df.shape[1] - 1
+        n_molecules = len(df)
+        all_digits = list(str(int(n_molecules * 0.9)))
+        first_digit = int(all_digits[0])
+        length = len(all_digits) - 1
+        if (n_molecules - first_digit * 10 ** length) < int(n_molecules * 0.1):
+            first_digit -= 1
+
+        data_size_for_all_loops = first_digit * 10 ** length
+
+        params = {
+            "name": chembl_name,
+            "data_file": data_file_path,
+            "data_width": data_width,
+            "save_model_per_epoch": save_model_per_epoch,
+            "reload_model": False,
+            "batchnorm_conv": True,
+            "batchnorm_mid": True,
+
+            "epochs": epochs,
+            "lr": 0.0001,
+            "RAND_SEED": 42,
+            "data_size_for_all_loops": data_size_for_all_loops,
+            # "loop_over_fit_batch_size": 500,
+            # "loop_over_fit_batch_id": 0,
+
+            "model_fit_batch_size": 100,
+            "paired_output": True,
+            "if_smiles": False,
+
+            "if_fp_one_way": True,
+            "hidden_dim": hidden_dim,
+            "fp_siamese_depth": fp_siamese_depth,
+            "fp_concat_depth": fp_concat_depth,
+            "fp_hidden_dim_reduction_rate": fp_hidden_dim_reduction_rate,
+            "fp_dropout_rate": 0.1,
+            "fp_activation": "relu",
+            "fp_loss_weight": fp_loss_weight,
+            "fp_n_activity_layers": fp_n_activity_layers,
+            "fp_activity_layer_size_scalar": fp_activity_layer_size_scalar,
+            "fp_activity_dropout_rate": 0.1,
+        }
+
+        training_params = ChemVAETrainingParams(**params)
+
+        metrics_per_dataset = main(training_params, eval_size)
+        metrics_per_dataset['dataset'] = chembl_name
+        metrics_per_dataset['eval_size'] = eval_size
+        metrics_all = pd.concat([metrics_all, metrics_per_dataset], ignore_index=True)
+
+    metrics_all.to_csv(f"chembl_oneway_metrics_batch_eval_size_{eval_size}.csv", index=False)
+
+
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
 
@@ -77,4 +154,19 @@ if __name__ == "__main__":
         level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
     )
     logging.info("Logging started.")
-    run_eval("./trained_models/chembl204/exp_oneway.json", 300)
+    # run_eval("./trained_models/chembl4016/exp_oneway.json", 50)
+
+    ### to run in batch:
+    root_dir = "./trained_models/chembl_data/"
+
+    chembl_info = pd.read_csv(
+        root_dir + "chembl_datasets_info.csv"
+    ).sort_values(by=["N(sample)"], ascending=False)
+    large_chembles = chembl_info[
+        (chembl_info["N(sample)"] > 900) & (chembl_info['Repetition Rate'] <= 0.15)]["File name"].tolist()
+
+    list_of_data_file = [root_dir + fr'{file_name}' for file_name in large_chembles]
+
+    run_eval_in_batch(
+        list_of_data_file, eval_size=100
+    )
